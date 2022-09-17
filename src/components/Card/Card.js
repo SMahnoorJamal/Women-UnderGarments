611eef5eddffb1367746c343f9cedd927143c282
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-17 18:49:54 +0500

    Star rating

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,12 @@ import {
     Card,
     Col, Button
 } from 'react-bootstrap';
+import ReactStars from "react-rating-stars-component";
 
+
+const ratingChanged = (newRating) => {
+    console.log(newRating);
+};
 function CardItem() {
 
     return (
@@ -14,61 +19,81 @@ function CardItem() {
                 height: '50%',
                 width: '47vw',
                 backgroundColor: 'maroon',
-                justifyContent:'space-between',
+                justifyContent: 'space-between',
                 flexDirection: 'row',
                 display: 'flex'
             }}>
-     
 
-                <div style={{
-                    width: '20.5vw', height: '80%', backgroundColor: 'greenyellow'
-                }}>
 
-                    <div style={{ width: '20vw', height: '80%', }}>
-                      
+            <div style={{
+                width: '20.5vw', height: '80%', backgroundColor: 'greenyellow'
+            }}>
+
+                <div style={{ width: '20vw', height: '80%', }}>
+
                     <img src={require('../../images/bra1.jpg')}
                         style={{
-                            border: `0.7px solid lightgray`, 
+                            border: `0.7px solid lightgray`,
                             height: '100%',
                             width: '20vw  ',
                             backgroundColor: 'gray'
                         }}
-                        alt=" bra1"/>
-                   </div>
-                    <div className='navbar1'>
+                        alt=" bra1" />
+                </div>
+                <div className='navbar1'>
 
-                        <label>Molded Cup Underwire Lace Bra & Bikni Set</label>
-                        <label style={{marginTop: '1vh'}}>Rs 1,850</label>
+                    <label>Molded Cup Underwire Lace Bra & Bikni Set</label>
+                    <label style={{ marginTop: '1vh' }}>Rs 1,850</label>
 
+
+                    <div>
+                        <ReactStars
+                            count={5}
+                            onChange={ratingChanged}
+                            size={9}
+                            activeColor="#ffd700"
+                        />
                     </div>
                 </div>
+            </div>
 
-            
 
-                <div style={{
-                    width: '20.5vw', height: '80%', backgroundColor: 'greenyellow'
-                }}>
 
-                    <div style={{ width: '20vw', height: '80%', }}>
-                      
+            <div style={{
+                width: '20.5vw', height: '80%', backgroundColor: 'greenyellow'
+            }}>
+
+                <div style={{ width: '20vw', height: '80%', }}>
+
                     <img src={require('../../images/bra1.jpg')}
                         style={{
-                            border: `1px solid lightgray`, 
+                            border: `1px solid lightgray`,
                             height: '100%',
                             width: '20vw  ',
                             backgroundColor: 'gray'
                         }}
-                        alt=" bra1"/>
-                   </div>
-                    <div className='navbar1'>
+                        alt=" bra1" />
+                </div>
+                <div className='navbar1'>
 
-                        <label>Molded Cup Underwire Lace Bra & Bikni Set</label>
-                        <label style={{marginTop: '1vh'}}>Rs 1,850</label>
+                    <label>Molded Cup Underwire Lace Bra & Bikni Set</label>
+                    <label style={{ marginTop: '1vh' }}>Rs 1,850</label>
 
+
+                    <div>
+                        <ReactStars
+                            count={5}
+                            onChange={ratingChanged}
+                            size={9}
+                            activeColor="#ffd700"
+                        />
                     </div>
                 </div>
 
-            
+
+            </div>
+
+
 
         </div>
     )
