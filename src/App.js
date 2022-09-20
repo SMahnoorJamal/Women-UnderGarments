2573fd761a94ca6f07ca6d2273323ee606b28947
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-20 15:02:39 +0500

    Grid Rows

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header/Header';
 import About from './screens/About';
 import Navbar from './components/Navbar';
 import Home from './screens/Home/Home';
+import OffsettingExample from '../src/components/Grid/Grid';
 
 //     //   <a
 //     //     className="App-link"
@@ -41,9 +42,11 @@ const AppWrapper = () => {
   return (
     // <  Card/>
     <Router>
+        <OffsettingExample/>
       {/* < Header /> */}
-    <Navbar /> 
-      <App /> 
+    {/* <Navbar /> 
+  
+      <App />  */}
     </Router>
   );
 };
