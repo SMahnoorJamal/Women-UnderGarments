51fa4ecd1dc85fc467866392fd87b66e17cf009d
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-21 22:17:44 +0500

    home screen Ui fixes

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -16,45 +16,47 @@ export default function HomeScreen() {
         <div style={{
             marginTop: 20,
             width: '95%',
-          marginLeft: '2.5vw',
+            marginLeft: '2.5vw',
             height: '100%',
-            backgroundColor:'green'
-            }}>
-    
-    {/* <Row> */}
-    <div style={{ marginTop: '17vh', flexDirection: 'row', display: 'flex' }}>
-                    <Card />
-    
-    {/* <Col> */}
-    <div style={{ backgroundColor: 'PINK', width: '16vw' }} >
-    
-    <label className='Title'>2022 BRA
-        COLLECTION</label>
-       
-    <div style={{ marginTop: '1vw' }}>
-        <text>
-            Check out new hottest styles.
-        </text>
-    </div> 
-    <button style={{ height: '6vh', width: '7vw', marginTop: '4vh' }}>SHOP NOW</button>
-    </div>
-    
-    <img
-                        className='bra2'
-                        variant="top" src={require('../../images/bra2.PNG')} alt=" bra2" />
-    
+            backgroundColor: 'green'
+          }}>
+      
+            {/* <Row> */}
+            <div style={{ marginTop: '17vh', flexDirection: 'row', display: 'flex' }}>
+              <Card />
+      
+              {/* <Col> */}
+              <div style={{ backgroundColor: 'PINK', width: '16vw', marginTop: '14vh', marginLeft: '5vw' }} >
+      
+                <label className='Title'>2022 BRA
+                  COLLECTION</label>
+      
+                <div style={{ marginTop: '1vw' }}>
+                  <text>
+                    Check out new hottest styles.
+                  </text>
                 </div>
-    
-    <br/>
-    
-        <div style={{ marginTop: '6vh' }}>
-                    <text >
-                        FEATURED PRODUCTS
-                    </text>
-    
-                    <Card />
-    
-                </div>
-        </div>
+                <button style={{ height: '6vh', width: '7vw', marginTop: '4vh' }}>SHOP NOW</button>
+              </div>
+      
+              <div style={{ marginLeft: '5vw' }}>
+                <img
+                  className='bra2'
+                  variant="top" src={require('../../images/bra2.PNG')} alt=" bra2" />
+              </div>
+      
+            </div>
+      
+            <br />
+      
+            <div style={{ marginTop: '6vh' }}>
+              <text >
+                FEATURED PRODUCTS
+              </text>
+      
+              <Card />
+      
+            </div>
+          </div>
     )
 }
