dd8cb02610b8b8e1b01451aa4cef8ea0cfd68d36
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-21 22:17:26 +0500

    home Screen ui fixes

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,54 +1,56 @@
 import React from 'react';
 
 import Card from '../components/Card/Card';
-  
+
 const About = () => {
   return (
 
-      <div style={{
-        marginTop: 20,
-        width: '95%',
+    <div style={{
+      marginTop: 20,
+      width: '95%',
       marginLeft: '2.5vw',
-        height: '100%',
-        backgroundColor:'green'
-        }}>
+      height: '100%',
+      backgroundColor: 'green'
+    }}>
 
-{/* <Row> */}
-<div style={{ marginTop: '17vh', flexDirection: 'row', display: 'flex' }}>
-                <Card />
+      {/* <Row> */}
+      <div style={{ marginTop: '17vh', flexDirection: 'row', display: 'flex' }}>
+        <Card />
 
-{/* <Col> */}
-<div style={{ backgroundColor: 'PINK', width: '16vw' }} >
+        {/* <Col> */}
+        <div style={{ backgroundColor: 'PINK', width: '16vw', marginTop: '14vh', marginLeft: '5vw' }} >
 
-<label className='Title'>2022 BRA
-    COLLECTION</label>
-   
-<div style={{ marginTop: '1vw' }}>
-    <text>
-        Check out new hottest styles.
-    </text>
-</div> 
-<button style={{ height: '6vh', width: '7vw', marginTop: '4vh' }}>SHOP NOW</button>
-</div>
+          <label className='Title'>2022 BRA
+            COLLECTION</label>
 
-<img
-                    className='bra2'
-                    variant="top" src={require('../images/bra2.PNG')} alt=" bra2" />
+          <div style={{ marginTop: '1vw' }}>
+            <text>
+              Check out new hottest styles.
+            </text>
+          </div>
+          <button style={{ height: '6vh', width: '7vw', marginTop: '4vh' }}>SHOP NOW</button>
+        </div>
 
-            </div>
+        <div style={{ marginLeft: '5vw' }}>
+          <img
+            className='bra2'
+            variant="top" src={require('../images/bra2.PNG')} alt=" bra2" />
+        </div>
 
-<br/>
+      </div>
 
-    <div style={{ marginTop: '6vh' }}>
-                <text >
-                    FEATURED PRODUCTS
-                </text>
+      <br />
 
-                <Card />
+      <div style={{ marginTop: '6vh' }}>
+        <text >
+          FEATURED PRODUCTS
+        </text>
 
-            </div>
+        <Card />
+
+      </div>
     </div>
   );
 };
-  
+
 export default About;
